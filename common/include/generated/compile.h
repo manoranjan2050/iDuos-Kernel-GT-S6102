598cfc87f83f61c1eaa2506f6b594efb26f84fd7
manoranjan <iduos@By-manoranjan2050.(none)>
2012-09-14 09:44:33 +0530

    Hello World

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 106 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#106 PREEMPT Wed Sep 12 21:56:34 IST 2012"
+#define LINUX_COMPILE_TIME "21:56:34"
+#define LINUX_COMPILE_BY "iDuos1.5Mini"
+#define LINUX_COMPILE_HOST "By-Manoranjan2050"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.6.3 (Sourcery CodeBench Lite 2012.03-57) "
